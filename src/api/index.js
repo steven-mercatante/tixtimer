66c92f04b3e7b5fe8555cf4dadd93425c8108764
Steven Mercatante <steven.mercatante@gmail.com>
2019-11-22 12:06:20 -0500

    implementing delete API calls

--- src/api/index.js
+++ src/api/index.js
@@ -8,33 +8,57 @@ const getHeaders = () => ({
   Authorization: "Token 5200a233be168d88abce47b302815f901d3da15e"
 });
 const apiRoot = "http://127.0.0.1:8000/";
+
 const _get = url =>
   axios
     .get(`${apiRoot}${url}`, { headers: getHeaders() })
     .then(resp => resp.data)
     .catch(err => console.log(`Error GET ${url}:`, err));
 
+const _post = (url, data) =>
+  axios
+    .post(`${apiRoot}${url}`, data, { headers: getHeaders() })
+    .then(resp => resp.data)
+    .catch(err => console.log(`Error GET ${url}:`, err));
+
+const _delete = url => {
+  axios
+    .delete(`${apiRoot}${url}`, { headers: getHeaders() })
+    .then(resp => {
+      return resp.data;
+    })
+    .catch(err => console.log(`Error GET ${url}:`, err));
+};
+
 export const getClients = () => _get("clients");
 
-export const createClient = name => {};
+export const createClient = name => _post("clients/", { name });
 
 export const getProjects = () => _get("projects");
 
 export const createProject = (clientId, name) => {};
 
-export const deleteProject = id => {};
+export const deleteProject = id => _delete(`projects/${id}/`);
 
-export const deleteProjects = ids => {};
+export const deleteProjects = ids => {
+  ids.forEach(id => {
+    deleteProject(id);
+  });
+};
 
 export const getTimers = () => _get("timers");
 
 export const createTimer = (id, now) => {};
 
-export const deleteClient = id => {};
+export const deleteClient = id => _delete(`clients/${id}/`);
 
-export const deleteClients = ids => {};
+export const deleteClients = ids => {
+  ids.forEach(id => {
+    deleteClient(id);
+  });
+};
 
-export const deleteTimer = id => {};
+export const deleteTimer = id => _delete(`timers/${id}/`);
 
 export const updateTimer = (id, data) => {};
 
