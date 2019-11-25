a637e80c284499e8c464991ce00649a7adcf3056
Steven Mercatante <steven.mercatante@gmail.com>
2019-11-25 12:24:56 -0500

    working on Timesheet editable table

--- src/components/Timesheet/Timesheet.js
+++ src/components/Timesheet/Timesheet.js
@@ -22,7 +22,7 @@ import format from "date-fns/format";
 import endOfMonth from "date-fns/endOfMonth";
 import parseISO from "date-fns/parseISO";
 import { observer } from "mobx-react";
-import { Table } from "antd";
+import { Form, Input, InputNumber, Table } from "antd";
 
 // TODO: clean up
 const _startOfMonth = parseISO(format(startOfMonth(new Date()), "yyyy-MM-dd"));
@@ -44,7 +44,7 @@ const _endOfLastWeek = parseISO(
   format(endOfWeek(addWeeks(new Date(), -1)), "yyyy-MM-dd")
 );
 
-const Timesheet = styled.div`
+const Container = styled.div`
   width: 100%;
   padding: 0 40px;
 `;
@@ -69,22 +69,115 @@ const Search = ({ setSearchTerm, setSearchTermTotalTime }) => {
   );
 };
 
-export default observer(function({ timesheetEntryStore, projects }) {
-  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
-  const { entries } = timesheetEntryStore;
+const EditableContext = React.createContext();
 
-  function assignEntryToProject(entryId, projectId) {
-    // updateEntry(entryId, { projectId });
-    updateTimesheetEntry(entryId, { projectId });
+function EditableCell(props) {
+  function getInput() {
+    if (props.inputType === "number") {
+      return <InputNumber />;
+    }
+    return <Input />;
+  }
+
+  function renderCell({ getFieldDecorator }) {
+    const {
+      editing,
+      dataIndex,
+      title,
+      inputType,
+      record,
+      index,
+      children,
+      ...restProps
+    } = props;
+    return (
+      <td {...restProps}>
+        {editing ? (
+          <Form.Item style={{ margin: 0 }}>
+            {getFieldDecorator(dataIndex, {
+              rules: [
+                {
+                  required: true,
+                  message: `Please Input ${title}!`
+                }
+              ],
+              initialValue: record[dataIndex]
+            })(getInput())}
+          </Form.Item>
+        ) : (
+          children
+        )}
+      </td>
+    );
+  }
+
+  return <EditableContext.Consumer>{renderCell}</EditableContext.Consumer>;
+}
+
+function EditableTable({ form, entries }) {
+  console.log("EditableTable.render()");
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [editingKey, setEditingKey] = useState("");
+  const isEditing = record => record.key === editingKey;
+
+  const edit = key => {
+    setEditingKey(key);
+  };
+
+  function handleSave(form, key) {
+    console.log("handleSave()");
+    form.validateFields((error, row) => {
+      if (error) {
+        return;
+      }
+      // TODO: check if any data changed before updating MST & DB
+      console.log("row:", row);
+    });
+    setEditingKey("");
   }
 
   const columns = [
     { title: "Client", dataIndex: "client" },
     { title: "Project", dataIndex: "project" },
-    { title: "Task", dataIndex: "task" },
-    { title: "Time", dataIndex: "time" },
-    { title: "Logged For", dataIndex: "loggedFor" }
-  ];
+    { title: "Task", dataIndex: "task", editable: true },
+    { title: "Time", dataIndex: "time", editable: true },
+    // { title: "Date", dataIndex: "date" },
+    {
+      title: "Action",
+      dataIndex: "action",
+      render: (text, record) => {
+        const editable = isEditing(record);
+        return editable ? (
+          <span>
+            <EditableContext.Consumer>
+              {form => (
+                <button onClick={() => handleSave(form, record.key)}>
+                  Save
+                </button>
+              )}
+            </EditableContext.Consumer>
+          </span>
+        ) : (
+          <button onClick={() => edit(record.key)}>Edit</button>
+        );
+      }
+    }
+  ].map(col => {
+    if (!col.editable) {
+      return col;
+    }
+
+    return {
+      ...col,
+      onCell: record => ({
+        record,
+        inputType: "text",
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record)
+      })
+    };
+  });
 
   const dataSource = entries.map(entry => ({
     key: entry.id,
@@ -92,7 +185,7 @@ export default observer(function({ timesheetEntryStore, projects }) {
     project: entry.project.name,
     task: entry.task,
     time: entry.seconds,
-    loggedFor: entry.loggedFor
+    date: entry.loggedFor
   }));
 
   const rowSelection = {
@@ -102,11 +195,35 @@ export default observer(function({ timesheetEntryStore, projects }) {
     }
   };
 
+  const components = {
+    body: {
+      cell: EditableCell
+    }
+  };
+
   return (
-    <Table
-      rowSelection={rowSelection}
-      columns={columns}
-      dataSource={dataSource}
-    ></Table>
+    <EditableContext.Provider value={form}>
+      <Table
+        components={components}
+        rowSelection={rowSelection}
+        columns={columns}
+        dataSource={dataSource}
+      ></Table>
+    </EditableContext.Provider>
   );
-});
+}
+
+function Timesheet({ form, timesheetEntryStore, projects }) {
+  console.log("Timesheet.render()");
+  const { entries } = timesheetEntryStore;
+
+  // function assignEntryToProject(entryId, projectId) {
+  //   // updateEntry(entryId, { projectId });
+  //   updateTimesheetEntry(entryId, { projectId });
+  // }
+
+  const EditableFormTable = observer(Form.create()(EditableTable));
+  return <EditableFormTable entries={entries} />;
+}
+
+export default observer(Timesheet);
